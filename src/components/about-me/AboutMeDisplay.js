4056446356e471c5153f95bdf86527e1df6e006a
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2020-03-21 10:06:34 +0200

    remove console.log

--- src/components/about-me/AboutMeDisplay.js
+++ src/components/about-me/AboutMeDisplay.js
@@ -10,20 +10,17 @@ export default ({
   imageAlt,
   ctaText,
   ctaHref
-}) => {
-  console.log(image);
-  return (
-    <div className="about-me">
-      <div className="about-me__text-outer">
-        <div className="about-me__text">
-          <div className="about-me__text-inner">
-            <h3 className="about-me__title">{title}</h3>
-            <div dangerouslySetInnerHTML={{ __html: text }} />
-            <Link to={ctaHref} className="about-me__cta btn">{ctaText}</Link>
-          </div>
+}) => (
+  <div className="about-me">
+    <div className="about-me__text-outer">
+      <div className="about-me__text">
+        <div className="about-me__text-inner">
+          <h3 className="about-me__title">{title}</h3>
+          <div dangerouslySetInnerHTML={{ __html: text }} />
+          <Link to={ctaHref} className="about-me__cta btn">{ctaText}</Link>
         </div>
       </div>
-      <Img alt={imageAlt} className="about-me__image" fluid={image.childImageSharp.fluid} />
     </div>
-  );
-}
+    <Img alt={imageAlt} className="about-me__image" fluid={image.childImageSharp.fluid} />
+  </div>
+);
