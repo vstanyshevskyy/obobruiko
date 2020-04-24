565fb469e5dadd44b2db9310ce64173b751ffb32
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2020-04-24 13:35:59 +0300

    Editable services

--- src/components/services-list/ServicesDisplay.js
+++ src/components/services-list/ServicesDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import ReactMarkdown from 'react-markdown';
 import './index.less';
 
 export default ({
@@ -13,21 +14,22 @@ export default ({
       <h3 className="services__title">{blockTitle}</h3>
       <p className="services-list__subtitle">{blockSubtitle}</p>
       {
-        services.map(({ title, subtitle, html, image, url }, i) => (
-          <div key="title" className="services-list__service">
+        services.map(({
+          title, text, image, linkText, link
+        }, i) => (
+          <div key={link} className="services-list__service">
             <div className={`services-list__service-image services-list__service-image--${i % 2 === 0 ? 'left' : 'right'}`}>
               <Img alt="" fluid={image.childImageSharp.fluid} />
             </div>
             <div className={`services-list__service-description services-list__service-description--${i % 2 === 0 ? 'right' : 'left'}`}>
-              <div className='services-list__service-description-inner'>
+              <div className="services-list__service-description-inner">
                 <h4 className="services-list__title">
                   {title}
                 </h4>
-                <div className="services-list__service-subtitle">
-                  {subtitle}
+                <div className="services-list__service-text">
+                  <ReactMarkdown source={text} />
                 </div>
-                <div className="services-list__service-text" dangerouslySetInnerHTML={{ __html: html }} />
-                <Link to={url} className="btn services-list__btn">Дізнатись більше</Link>
+                <Link to={link} className="btn services-list__btn">{linkText}</Link>
               </div>
             </div>
           </div>
--- src/components/services-list/index.js
+++ src/components/services-list/index.js
@@ -7,104 +7,38 @@ export default () => (
   <StaticQuery
     query={graphql`
       query ServicesQuery {
-        image1: file(relativePath: { eq: "uploads/individual.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 900) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        image2: file(relativePath: { eq: "uploads/couples.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 900) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        image3: file(relativePath: { eq: "uploads/workshops.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 900) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        hero: allMarkdownRemark(filter: { frontmatter:  { contentType: { eq: "footer_settings"} }}){
-          edges{
-            node{
-              frontmatter{
-                links {
-                  text
-                  url
+        services: markdownRemark(frontmatter: {
+          contentType: { eq: "homepageServices" }
+        }){
+          frontmatter {
+            title
+            subtitle
+            services {
+              title
+              text
+              image{
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 960, srcSetBreakpoints: [768]) {
+                    ...GatsbyImageSharpFluid
+                  }
                 }
               }
+              linkText
+              link
             }
           }
         }
       }
     `}
     render={({
-      title = 'Послуги',
-      subtitle = 'With You Every Step of the Way',
-      image1,
-      image2,
-      image3,
-      services = [{
-        image: image1,
-        title: 'Індивідуальне консультування',
-        subtitle: '1 година в тиждень для себе',
-        html: `
-          <p>Ми можемо попрацювати над:</p>
-          <ul>
-            <li>Підвищенням самооцінки
-            <li>Пошуком внутрішніх бажань і ресурсів
-            <li>Подоланням страхів
-            <li>Прийняттям рішень​
-            <li>Постановкою та досягненням цілей
-          </ul>
-          <p>
-          Усе, що потрібно вже є всередині вас.<br />
-          Разом, ми можемо знайти та подолати бар'єри,<br />
-          що перешкоджають досягненню ваших цілей.
-          </p>
-          `,
-        url: '/services/individual-counseling'
-      }, {
-        image: image2,
-        title: 'Парні консультації',
-        subtitle: '1,5 години в тиждень для вашої пари',
-        html: `
-        <p>Я можу допомогти вам і вашому партнеру:</p>
-        <ul>
-          <li>навчитись говорити про те, що не влаштовує, так, щоб слова кожного з вас були почуті, а тарілки залишились цілими
-          <li>вирішити конфлікти
-          <li>відновити взаєморозуміння
-          <li>повернути довіру та близькість у стосунки
-          <li>підтримувати теплі взаємини й надалі
-        </ul>
-        <p>Побудувати теплі надійні стосунки, сповнені любові – можливо. Я покажу вам як саме.</p>
-        `,
-        url: '/services/couple-counseling'
-      }, {
-        image: image3,
-        title: 'Майстер-класи',
-        subtitle: '2+ години для вашої команди',
-        html: `
-        <p>Ви дізнаєтесь:</p>
-        <ul>
-        <li>
-        які є стилі спілкування, переваги та недоліки кожного зі стилів. А також який стиль ви використовуєте найчастіше
-        <li>
-        як відмовляти асертивно і потренуєтесь це робити
-        <li>
-        яка користь від позитивного відгуку і попрактикуєтесь його давати
-        <li>
-        навіщо потрібен корегуючий відгук і як висловлювати його так, щоб він був доречним, а головне корисним. І звісно ж повправляєтесь давати такий відгук
-        </ul>
-
-        <p>Давайте разом покращувати комунікацію у вашій команді.</p>
-        `,
-        url: '/services/teams-workshops'
-      }]
+      services: {
+        frontmatter: {
+          title,
+          subtitle,
+          services
+        }
+      }
     }) => (
       <ServicesDisplay
         title={title}
