9746aec0b2451f8ecb295a3d3c2a1c32fd09f31b
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2021-05-13 20:26:55 +0300

    Update contact form

--- src/components/contact-form/contactFormDisplay.js
+++ src/components/contact-form/contactFormDisplay.js
@@ -54,11 +54,13 @@ const ContactForm = ({
             icons={[
               {type: 'Facebook', url: 'https://www.facebook.com/psychologist.olesya.bobruyko'},
               {type: 'Instagram', url: 'https://www.instagram.com/olesya.bobruyko/'},
+              {type: 'Skype', url: 'https://join.skype.com/invite/mwn1okh3jRcF'},
               {type: 'Mail', url: 'mailto:psychologist.olesya.b@gmail.com'},
+              {type: 'Youtube', url: 'https://www.youtube.com/channel/UCax4hKxA_XE-rkvVFI4UJQQ'},
             ]}
           />
       </div>
-      {
+      {/* {
         isSent
           ? (
             <p className="contact-form__sent-message">
@@ -82,7 +84,7 @@ const ContactForm = ({
               </button>
             </form>
           )
-      }
+      } */}
 
     </div>
   );
