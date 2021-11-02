3586979cc033b4de81c130e9ade7e30b31b59e31
vstanyshevskyy <vitaliy@pleo.io>
2021-11-02 09:26:08 +0200

    Add questionaries to admin

--- src/cms/config/collections/index.js
+++ src/cms/config/collections/index.js
@@ -1,11 +1,13 @@
 import pages from './pages';
 import articles from './articles';
+import questionaries from './questionaries';
 import settings from './settings';
 import homepage from './homepage';
 
 const collections = [
   pages,
   articles,
+  questionaries,
   settings,
   homepage
 ];
---
+++ src/cms/config/collections/questionaries.js
@@ -0,0 +1,143 @@
+import languages from './languageSelect';
+
+const articles = {
+  name: 'questionaries',
+  label: 'Опитувальники',
+  folder: 'content/questionaries',
+  create: true,
+  fields: [
+    {
+      label: 'Заголовок для адмінки',
+      name: 'title',
+      widget: 'string'
+    },
+    {
+      label: 'Контент',
+      name: 'content',
+      widget: 'list',
+      fields: [
+        languages,
+        {
+          label: 'Url',
+          name: 'path',
+          widget: 'string',
+          hint: '/questionaries/my-questionary',
+          pattern: ['^\\/questionaries\\/[a-zA-Z0-9-]*', 'урл не валідний']
+        },
+        {
+          label: 'Заголовок',
+          name: 'title',
+          widget: 'string'
+        },
+        {
+          label: 'Опис',
+          name: 'description',
+          widget: 'markdown',
+          required: false
+        },
+        {
+          label: 'Інструкції',
+          name: 'instruction',
+          widget: 'markdown',
+          required: false
+        },
+        {
+          label: 'Питання',
+          name: 'questions',
+          widget: 'list',
+          required: false,
+          fields: [
+            {
+              label: 'Текст',
+              name: 'text',
+              widget: 'string'
+            },
+            {
+              label: 'Мінімальне значення, щоб показати',
+              name: 'minScore',
+              widget: 'number',
+              required: false
+            },
+            {
+              label: 'Відповіді',
+              name: 'answers',
+              widget: 'list',
+              fields: [
+                {
+                  label: 'Текст',
+                  name: 'text',
+                  widget: 'string'
+                },
+                {
+                  label: 'Значення',
+                  name: 'value',
+                  widget: 'number'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Результати',
+          name: 'results',
+          widget: 'list',
+          fields: [
+            {
+              label: 'Текст',
+              name: 'text',
+              widget: 'string'
+            },
+            {
+              label: 'Мінімальний результат',
+              name: 'minScore',
+              widget: 'number'
+            },
+            {
+              label: 'Максимальний результат',
+              name: 'maxScore',
+              widget: 'number'
+            }
+          ]
+        },
+        {
+          label: 'Фото для шерінгу',
+          name: 'image',
+          widget: 'image',
+          required: false
+        },
+        {
+          label: 'Показувати білий фон для навігації',
+          name: 'useWhiteForNav',
+          widget: 'boolean',
+          default: false
+        },
+        {
+          label: 'Час публікації',
+          name: 'publishTime',
+          required: false,
+          widget: 'datetime'
+        },
+        {
+          label: 'Meta Keywords',
+          name: 'metaKeywords',
+          widget: 'string',
+          required: false
+        },
+        {
+          label: 'Meta Description',
+          name: 'metaDescription',
+          widget: 'string',
+          required: false
+        },
+        {
+          label: 'FB Description',
+          name: 'fbDescription',
+          widget: 'string',
+          required: false
+        }
+      ]
+    }
+  ]
+};
+
+export default articles;
