8d21e9a6dde0a0e00a9b965a0672fe1bb2b5f326
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2024-03-18 16:12:04 +0100

    Try again to fix the images

--- src/components/markdown/index.jsx
+++ src/components/markdown/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import ImageRenderer from './image-renderer';
 
-export default props => <ReactMarkdown {...props} components={{ image: ImageRenderer }} />;
+export default props => <ReactMarkdown {...props} components={{ img: ImageRenderer }} />;
