5deef59dd3ae68e5e166101b0e5b21b079ccd712
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@awaze.com>
2026-01-08 20:34:03 +0100

    Remove answers component. Simplify the structure

--- src/templates/questionnaires/components/question/index.jsx
+++ src/templates/questionnaires/components/question/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Answers from '../answers';
+import Answer from '../answer';
 import './index.less';
 
 const Question = ({ question }) => (
   <div className="question">
     <label className="questionText">{question.questionText}</label>
-    <Answers question={question} />
+    <div className="answers">
+      {question.answers.map(answer => (
+        <Answer key={answer.id} answer={answer} questionId={question.id} />
+      ))}
+    </div>
   </div>
 );
 
