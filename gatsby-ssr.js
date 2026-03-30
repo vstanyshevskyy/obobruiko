2da653ea89d3e03473cd7c1aff68637f4e5fa1b8
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2026-03-30 22:20:08 +0200

    Optimize fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -14,36 +14,10 @@ exports.onRenderBody = ({ setHeadComponents, pathname }) => {
   }
 
   setHeadComponents([
-    React.createElement('link', {
-      key: 'raleway-font',
-      href: 'https://fonts.googleapis.com/css2?family=Raleway:wght@400;500&display=swap',
-      rel: 'stylesheet'
-    }),
     React.createElement('script', {
       key: 'termly-script',
       type: 'text/javascript',
       src: 'https://app.termly.io/resource-blocker/775390f3-9116-4690-bfe3-e10982a065ca?autoBlock=off'
     })
   ]);
-};
-
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents, pathname }) => {
-  if (isAdminPath(pathname)) {
-    return;
-  }
-
-  const headComponents = getHeadComponents();
-  
-  // Move font preloads to the beginning for faster loading
-  headComponents.sort((x, y) => {
-    if (x.key === 'raleway-font' || x.key === 'opensans-font') {
-      return -1;
-    }
-    if (y.key === 'raleway-font' || y.key === 'opensans-font') {
-      return 1;
-    }
-    return 0;
-  });
-
-  replaceHeadComponents(headComponents);
 };
